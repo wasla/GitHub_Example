0df716ed01cdc4b3eb47f642dfa866b738a17a94
wasla <Wasla.Habib@student.pxl.be>
2017-01-03 21:17:35 +0100

    Made some changes in bob.java files

--- Bob.java
+++ Bob.java
@@ -2,6 +2,6 @@
 public class Bob {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Bob Wishing you a very happy new year with a hope that you will have many blessing in the year to come");
+		System.out.println("Hello Bob ");
 	}
 }

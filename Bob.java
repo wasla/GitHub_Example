f5623ceabbaf32e82676c77f1015d01a94aac766
wasla <Wasla.Habib@student.pxl.be>
2017-01-03 21:09:57 +0100

    Made three files Bob, foo , Hello_rename

---
+++ Bob.java
@@ -0,0 +1,7 @@
+
+public class Bob {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Bob Wishing you a very happy new year with a hope that you will have many blessing in the year to come");
+	}
+}

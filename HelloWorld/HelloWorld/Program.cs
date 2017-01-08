560e9e5bd0c1522f0e1a270153546fdc0bffbceb
wasla <Wasla.Habib@student.pxl.be>
2017-01-08 22:01:42 +0100

    merge test

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloooooooooooooo World");
+            Console.WriteLine("Hell World");
             Console.WriteLine("Helloo");
 
             Console.ReadKey(); 
